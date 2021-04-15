ee741cb376cc294647ffeb75855e79dd2d910d97
Michal Orlik <sogehige@gmail.com>
2021-04-15 11:37:44 +0200

    feat(permissions): add defaultPermissions

---
+++ permissions/defaultPermissions.d.ts
@@ -0,0 +1,9 @@
+declare const defaultPermissions: {
+    readonly CASTERS: "4300ed23-dca0-4ed9-8014-f5f2f7af55a9";
+    readonly MODERATORS: "b38c5adb-e912-47e3-937a-89fabd12393a";
+    readonly SUBSCRIBERS: "e3b557e7-c26a-433c-a183-e56c11003ab7";
+    readonly VIP: "e8490e6e-81ea-400a-b93f-57f55aad8e31";
+    readonly FOLLOWERS: "c168a63b-aded-4a90-978f-ed357e95b0d2";
+    readonly VIEWERS: "0efd7b1c-e460-4167-8e06-8aaf2c170311";
+};
+export { defaultPermissions };
