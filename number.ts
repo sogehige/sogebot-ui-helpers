a7db84f28a5002e60e574940caedaaa4a63dac80
Michal Orlik <sogehige@gmail.com>
2021-09-22 13:34:15 +0200

    fix(number): return decimals as empty string if undefined

---
+++ number.ts
@@ -0,0 +1,28 @@
+function getDecimals (value: string | number) {
+  return String(value).split('.')[1] ?? '';
+}
+
+function format (thousandsDelimiter = '', decimalsDelimiter = '.', precision = 2) {
+  return (value: number): string => {
+    let generatedNumber = '';
+    const decimals = getDecimals(value);
+    const decimalsLength = decimals.length > 0 ? decimals.length + 1 : 0;
+
+    for (let i = 0; i < String(value).length - decimalsLength; i++) {
+      const idx = (String(value).length - 1) - i - decimalsLength;
+      generatedNumber += String(value)[idx];
+      if (i % 3 === 2 && (i + 1) !== String(value).length - decimalsLength && i !== 0) {
+        generatedNumber += thousandsDelimiter;
+      }
+    }
+
+    generatedNumber = generatedNumber.split('').reverse().join('');
+
+    if (decimalsLength > 0 && precision > 0) {
+      generatedNumber += decimalsDelimiter + (getDecimals(Number.parseFloat(`0.${decimals}`).toFixed(precision)));
+    }
+    return generatedNumber;
+  };
+}
+
+export { getDecimals, format }
